c459efeb8ed2a6f21056bbc53cdc852707e92098
Blix <sblixenkrone@me.com>
2018-06-22 17:47:54 +0200

    Videogular2 implementation and model video class

---
+++ src/app/shared/models/video.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { VgAPI } from 'videogular2/core';
+
+@Component({
+  selector: 'app-video',
+  template:
+    `<!--- VIDEO HTML -->
+
+    <vg-player (onPlayerReady)="onPlayerReady($event)" autoplay>
+      <video [vgMedia]="media" #media id="singleVideo" preload="auto">
+        <source src="{{inputSrc}}" autoplay="true" type="video/mp4">
+      </video>
+    </vg-player>
+
+    <!--- VIDEO HTML -->`,
+  styles: []
+})
+
+export class VideoComponent implements OnInit {
+  @Input() inputSrc: any;
+  vgApi: VgAPI;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onPlayerReady(api: VgAPI) {
+    this.vgApi = api;
+    this.vgApi.getDefaultMedia().subscriptions.canPlay
+      .subscribe(() => {
+        this.vgApi.play();
+      })
+    this.vgApi.getDefaultMedia().subscriptions.ended
+      .subscribe(() => {
+        this.vgApi.play();
+      })
+  }
+
+
+}
